f69679a2fd2b6f569f73eb661a20c640d5c4baa3
srikanth <srikanth@DESKTOP-47E3GQR>
2023-11-29 13:35:50 +0530

    Add project files.

---
+++ studentreg/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+
+
+
+namespace studentreg
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load1();
+          
+
+        }
+
+
+
+        //      SqlConnection con = new SqlConnection("Data Source=.\\sqlexpress;Initial Catalog=gcbt;Integrated Security=True");
+
+        MySqlConnection con = new MySqlConnection(" Server = 68.178.155.64 ; Port = 3306; Database = gcbt; Uid = gcbt; Pwd = y]TpqISq0zAQ; ");
+        MySqlCommand cmd;
+        MySqlDataReader read;
+        MySqlDataAdapter drr;
+        String id;
+        bool Mode = true;
+        string sql;
+
+        
+
+
+
+
+
+        public void Load1()
+        {
+            
+            try
+            {
+              
+                sql = "select * from student";
+                cmd = new MySqlCommand(sql, con);
+                con.Open();
+
+                read = cmd.ExecuteReader();
+                dataGridView1.Rows.Clear();
+
+                while (read.Read())
+                {
+
+                    dataGridView1.Rows.Add(read[0],read[1],read[2],read[3]);
+
+                }
+                con.Close();
+
+
+
+            }
+            catch(Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+        }
+
+
+
+       public void getID(String id)
+        {
+            sql = "select * from student where id= '" + id + "' ";
+
+            cmd = new MySqlCommand(sql, con);
+            con.Open();
+            read= cmd.ExecuteReader();
+            while (read.Read())
+            {
+                txtName.Text = read[1].ToString();
+                txtCourse.Text = read[2].ToString();
+                txtFee.Text = read[3].ToString();
+
+            }
+            con.Close();
+
+        }
+
+
+        
+
+
+        
+         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }   
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string course = txtCourse.Text;
+            string fee = txtFee.Text;
+
+            if (Mode == true)
+            {
+
+                sql = "insert into student(stname, course, fee) values(@stname, @course,@fee)";
+                con.Open();
+                cmd = new MySqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@stname", name);
+                cmd.Parameters.AddWithValue("@course", course);
+                cmd.Parameters.AddWithValue("@fee", fee);
+                MessageBox.Show("Record added");
+                cmd.ExecuteNonQuery();
+
+                txtName.Clear();
+                txtCourse.Clear();
+                txtFee.Clear();
+                txtName.Focus();
+                
+
+            }
+            else
+            {
+
+                id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                sql = "update student set stname = @stname,course = @course, fee = @fee where id = @id";
+                con.Open();
+                cmd = new MySqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@stname", name);
+                cmd.Parameters.AddWithValue("@course", course);
+                cmd.Parameters.AddWithValue("@fee", fee);
+                cmd.Parameters.AddWithValue("@id", id);
+                MessageBox.Show("Record Updated");
+                cmd.ExecuteNonQuery();
+
+                txtName.Clear();
+                txtCourse.Clear();
+                txtFee.Clear();
+                txtName.Focus();
+                button1.Text = "Save";
+                Mode = true;
+
+
+
+
+            }
+
+            con.Close();
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex == dataGridView1.Columns["Edit"].Index && e.RowIndex >= 0)
+            {
+                Mode = false;
+                id= dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                getID(id);
+                button1.Text = "Edit";
+            }
+
+            else if (e.ColumnIndex == dataGridView1.Columns["Delete"].Index && e.RowIndex >= 0)
+            {
+                Mode = false;
+                id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                sql = "delete from student where id= @id";
+                con.Open();
+                cmd = new MySqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record Deleted");
+                con.Close();
+                               
+                
+            }
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtCourse.Clear();
+            txtFee.Clear();
+            txtName.Focus();
+            button1.Text = "Save";
+            Mode = true;
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Load1();
+        }
+    }
+}
---
+++ studentreg/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("studentreg")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("studentreg")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d8ec0163-6a20-4b89-9a48-07fa6b5d0162")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
